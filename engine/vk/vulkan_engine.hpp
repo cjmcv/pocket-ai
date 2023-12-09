c3474932ab6158e11d4cffef1915e3694b53863e
cjmcv <398342899@qq.com>
2023-12-09 14:03:03 +0000

    [vk] 1. Change the layout to be set by the specialization constant, specifying 0->x, 1->y, 2->z;  2. Delete KernelDispatch, simplify the kernel registration process, and leave it to the caller for registration.

--- engine/vk/vulkan_engine.hpp
+++ engine/vk/vulkan_engine.hpp
@@ -20,9 +20,6 @@
 namespace ptk {
 namespace vk {
 
-// 该文件无vulkan依赖，kernel中不带入vulkan相关内容，与vulkan engine解耦。
-// kernel的配置只描述了kernel的相关信息，engine里include了kernels_list后将里面的配置转换为vulkan相关类型进行使用。
-// vulkan -> op 依赖 engine 依赖 kernel+kernel dispatcher。 host -> op 依赖 kernel。
 typedef enum DescriptorType {
     DESCRIPTOR_TYPE_STORAGE_IMAGE = 1, // 3,
     DESCRIPTOR_TYPE_UNIFORM_BUFFER = 2, // 6,
@@ -43,106 +40,25 @@ struct KernelParams {
     std::vector<DescriptorType> buffer_type;
     std::vector<SpecializationConstant> spec_constant;
     uint32_t push_constant_num;
-    uint32_t workgroup_size[3];
 };
-
-// TODO: 删掉，在test里有
-const int WIDTH = 3200; // Size of rendered mandelbrot set.
-const int HEIGHT = 2400; // Size of renderered mandelbrot set.
-
-// TODO：换个地方
-void SetParamsMandelbrot(KernelParams *params) {
-    params->buffer_type = {
-        DESCRIPTOR_TYPE_STORAGE_BUFFER
-    };
-    params->spec_constant = {};
-    params->push_constant_num = 0;
-    // The workgroup_size is fixed in comp: layout(local_size_x = WG_X, local_size_y = WG_Y, local_size_z = 1) in;
-    // 这里的设定只是计算dispatch的group数量，考虑用特化常量
-    params->workgroup_size[0] = 32;
-    params->workgroup_size[1] = 32;
-    params->workgroup_size[2] = 1;
-}
-
-void SetParamsMatMulTiledFp32(KernelParams *params) {
-    params->buffer_type = {
-        DESCRIPTOR_TYPE_STORAGE_BUFFER,
-        DESCRIPTOR_TYPE_STORAGE_BUFFER,
-        DESCRIPTOR_TYPE_STORAGE_BUFFER // VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER
-    };
-
-    params->spec_constant = {
-        {0, 640},
-        {1, 640},
-        {2, 640},
-    };
-    params->push_constant_num = 0;
-    params->workgroup_size[0] = 16;
-    params->workgroup_size[1] = 1;
-    params->workgroup_size[2] = 1;
-}
-
-void SetParamsEngineTest(KernelParams *params) {
-    params->buffer_type = {
-        DESCRIPTOR_TYPE_STORAGE_BUFFER,
-        DESCRIPTOR_TYPE_STORAGE_BUFFER,
-        DESCRIPTOR_TYPE_STORAGE_BUFFER // VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER
-    };
-
-    params->spec_constant = {
-        {0, 160},
-        {1, 320}};
-    SpecializationConstant c;
-    c.id = 2;
-    c.value.f32 = 640.123f;
-    params->spec_constant.push_back(c);
-
-    params->push_constant_num = 2;
-    params->workgroup_size[0] = 16;
-    params->workgroup_size[1] = 1;
-    params->workgroup_size[2] = 1;
-}
 
 typedef void (*SetpParamsFuncs)(KernelParams *params);
-
-class VulkanKernelDispatcher {
-public:
-    //
-    KernelParams *CreateKernelParams(std::string kernel_name) {
-        KernelParams *params = new KernelParams;
-
-        std::map<std::string, SetpParamsFuncs>::iterator iter;
-        iter = params_.find(kernel_name);
-        if(iter != params_.end()) {
-            PTK_LOGI("CreateKernelParams: %s.\n", kernel_name.c_str());
-            iter->second(params);
-        }
-        else {
-            printf("Can not find params.\n");
-        }
-        return params;
-    }
-	// Singleton mode. Only one KernelFactory exist.
-	static VulkanKernelDispatcher *GetInstance() {
-		static VulkanKernelDispatcher *dispatcher = new VulkanKernelDispatcher;
-		return dispatcher;
-	}
-
-private:
-	VulkanKernelDispatcher() {
-        params_["mandelbrot"] = SetParamsMandelbrot;
-        params_["matmul_tiled_fp32"] = SetParamsMatMulTiledFp32;
-        params_["engine_test"] = SetParamsEngineTest;
-    }
-
-    std::map<std::string, SetpParamsFuncs> params_;
-};
 
 class VulkanEngine {
     // kernel运行所需资源单元。由Engine创建，一个kernel对应一个ExecUnit
     class ExecUnit {
     public:
-        void Run(std::vector<Buffer*> &input_buffers, const int push_constant_size, 
+        void GetGroupCount(const uint32_t width, const uint32_t height, const uint32_t channels) {
+            uint32_t size_x = params_->spec_constant[0].value.u32;
+            uint32_t size_y = params_->spec_constant[1].value.u32;
+            uint32_t size_z = params_->spec_constant[2].value.u32;
+
+            group_count_xyz_[0] = (width + size_x - 1) / size_x;
+            group_count_xyz_[1] = (height + size_y - 1) / size_y;
+            group_count_xyz_[2] = (channels + size_z - 1) / size_z;
+        }
+
+        void Run(uint32_t *num_xyz, std::vector<Buffer*> &input_buffers, const int push_constant_size, 
                  const void *push_constant, std::vector<Buffer*> &output_buffers) {
             static int idx = 0;
             idx++;
@@ -158,15 +74,17 @@ class VulkanEngine {
             command_buffer_->BindPipelineAndDescriptorSets(pipeline_, {descriptor_set_});
             if (push_constant_size != 0)
                 command_buffer_->PushConstant(pipeline_->pipeline_layout(), push_constant_size, push_constant);
-            command_buffer_->Dispatch((uint32_t)ceil(WIDTH / float(params_->workgroup_size[0])), 
-                                      (uint32_t)ceil(HEIGHT / float(params_->workgroup_size[1])), 
-                                      1);
+
+            GetGroupCount(num_xyz[0], num_xyz[1], num_xyz[2]);
+            command_buffer_->Dispatch(group_count_xyz_[0], group_count_xyz_[1], group_count_xyz_[2]);
             command_buffer_->End();
 
             device_->QueueSubmitAndWait(command_buffer_->command_buffer());
         }
 
         KernelParams *params_;
+        uint32_t group_count_xyz_[3];
+
         Device *device_;
         ShaderModule *shader_module_;
         //
@@ -178,13 +96,21 @@ class VulkanEngine {
     };
 
 public:
-    void Init(std::string shaders_path, int physical_device_id = 0, bool enable_validation = false) {
-        vk_dispatcher_ = VulkanKernelDispatcher::GetInstance();
+    void Init(std::string shaders_path, std::vector<std::pair<std::string, SetpParamsFuncs>> &shaders_params, 
+              int physical_device_id = 0, bool enable_validation = false) {
+        // vk_dispatcher_ = VulkanKernelDispatcher::GetInstance();
         instance_ = new Instance(enable_validation);
         std::vector<VkPhysicalDevice> phys_devices = instance_->EnumeratePhysicalDevices(true);
         device_ = Device::Create(phys_devices[physical_device_id], VK_QUEUE_COMPUTE_BIT, instance_->layers());
 
-        SetKernelMap();
+        // Register
+        for (uint32_t i=0; i<shaders_params.size(); i++) {
+            ExecUnit *res = new ExecUnit;
+            res->params_ = new KernelParams;
+            shaders_params[i].second(res->params_);
+            exec_map_[shaders_params[i].first] = res;
+        }
+        // Setup
         std::unordered_map<std::string, ExecUnit*>::iterator it = exec_map_.begin();
         while (it != exec_map_.end()) {
             ExecUnit *res = it->second;
@@ -247,7 +173,7 @@ public:
                             size);
     }
 
-    void Run(std::string kernel_name, std::vector<Buffer*> &input_buffers, 
+    void Run(std::string kernel_name, uint32_t *num_xyz, std::vector<Buffer*> &input_buffers, 
              const int push_constant_size, const void *push_constant, 
              std::vector<Buffer*> &output_buffers) {
 
@@ -258,26 +184,7 @@ public:
             return;
         }
         ExecUnit *res = it->second;
-        res->Run(input_buffers, push_constant_size, push_constant, output_buffers);
-    }
-
-private:
-    void SetKernelMap() {
-        {
-            ExecUnit *res = new ExecUnit;
-            res->params_ = vk_dispatcher_->CreateKernelParams("mandelbrot");
-            exec_map_["mandelbrot"] = res;
-        }
-        {
-            ExecUnit *res = new ExecUnit;
-            res->params_ = vk_dispatcher_->CreateKernelParams("matmul_tiled_fp32");
-            exec_map_["matmul_tiled_fp32"] = res;
-        }
-        {
-            ExecUnit *res = new ExecUnit;
-            res->params_ = vk_dispatcher_->CreateKernelParams("engine_test");
-            exec_map_["engine_test"] = res;
-        }
+        res->Run(num_xyz, input_buffers, push_constant_size, push_constant, output_buffers);
     }
 
 private:
@@ -285,7 +192,6 @@ private:
     Device *device_;
 
     std::unordered_map<std::string, ExecUnit*> exec_map_;
-    VulkanKernelDispatcher *vk_dispatcher_;
 };
 
 } // namespace vk
