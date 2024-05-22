e86ab91ad28448532ff43085461cb4f6d88e69dc
cjmcv <398342899@qq.com>
2024-05-22 15:38:56 +0000

    [vk] Add gemm v3.

--- example/vk/comp2spv.bat
+++ example/vk/comp2spv.bat
@@ -1,2 +1,11 @@
-D:\software\VulkanSDK\1.3.231.1\Bin\glslangValidator.exe -V shaders\gemm_v1.comp -o shaders/spv/gemm_v1.spv
+pushd shaders
+md spv
+popd
+
+set GLSLANG_VALIDATIOR_PATH=D:\software\VulkanSDK\1.3.231.1\Bin
+
+%GLSLANG_VALIDATIOR_PATH%\glslangValidator.exe -V shaders\engine_test.comp -o shaders/spv/engine_test.spv
+%GLSLANG_VALIDATIOR_PATH%\glslangValidator.exe -V shaders\gemm_v1.comp -o shaders/spv/gemm_v1.spv
+%GLSLANG_VALIDATIOR_PATH%\glslangValidator.exe -V shaders\gemm_v2.comp -o shaders/spv/gemm_v2.spv
+%GLSLANG_VALIDATIOR_PATH%\glslangValidator.exe -V shaders\gemm_v3.comp -o shaders/spv/gemm_v3.spv
 pause
