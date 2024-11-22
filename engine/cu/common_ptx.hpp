0f069bad6cc54cf08e8ee8c402b179559a681656
cjmcv <398342899@qq.com>
2024-11-23 00:13:32 +0800

    [cu] Update common_ptx.

--- engine/cu/common_ptx.hpp
+++ engine/cu/common_ptx.hpp
@@ -1,7 +1,8 @@
 // %%cuda_group_save --group shared --name "common_mma.h"
+// https://docs.nvidia.com/cuda/parallel-thread-execution/index.html
 
-#ifndef POCKET_AI_ENGINE_CUDA_COMMON_MMA_HPP_
-#define POCKET_AI_ENGINE_CUDA_COMMON_MMA_HPP_
+#ifndef POCKET_AI_ENGINE_CUDA_COMMON_PTX_HPP_
+#define POCKET_AI_ENGINE_CUDA_COMMON_PTX_HPP_
 
 #include "pocket-ai/engine/cu/common.hpp"
 //#include "common.h"
@@ -18,7 +19,16 @@ namespace cu {
 #define CP_ASYNC_CG(dst, src, bytes) \
     asm volatile("cp.async.cg.shared.global [%0], [%1], %2;\n" ::"r"(dst), "l"(src), "n"(bytes))
 
+#define CP_ASYNC_COMMIT_GROUP() \
+    asm volatile("cp.async.commit_group;\n" ::)
+
+#define CP_ASYNC_WAIT_ALL() \
+    asm volatile("cp.async.wait_all;\n" ::)
+
+#define CP_ASYNC_WAIT_GROUP(n) \
+    asm volatile("cp.async.wait_group %0;\n" ::"n"(n))
+
 } // namespace cu
 } // namespace pai
 
-#endif //POCKET_AI_ENGINE_CUDA_COMMON_MMA_HPP_
+#endif //POCKET_AI_ENGINE_CUDA_COMMON_PTX_HPP_
